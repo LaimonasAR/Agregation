8721a9db935900d5bc36cd952b206b8beaa03064
LaimonasAR <laimonas.paura@icloud.com>
2023-06-22 19:38:35 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+""" 
+#-----------Description--------------
+
+Online store "Target" has a database containing their sales data, containing:
+Product names;
+Quantities sold to customer;
+Price;
+Customer;
+date of purchase;
+
+Task is to analyze given data to:
+
+1. List all products sold in April.
+2. Find the customer with most purchasses.
+3. Gather all porducts purchased by that customer.
+4. Find the highest selling product.
+
+#-----------Tasks---------------------
+
+1. Create database for store "Target" and collection "sales"
+2. Populate "sales" collection with data.
+3. Write aggregation pipelines for:
+    3.1 Listing all products sold in April.
+    3.2 Finding customer with most purchases.
+    3.3 Gathering all products purchased by that customer
+    3.4 Finding the highest selling product.
+
+"""
+
+
